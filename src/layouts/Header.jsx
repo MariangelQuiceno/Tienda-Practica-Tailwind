ba0fe25984c22f5b70f69c28c058d82dd16d225d
Mariángel <126704306+MariangelQuiceno@users.noreply.github.com>
2024-05-07 17:20:36 -0500

    Añadido Carrusel Simple

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import adHeader from '../assets/ads/ad-1-header.png'
+import React, { useState, useEffect } from 'react';
+import ad1 from '../assets/ads/ad-1-header.png';
+import ad2 from '../assets/ads/ad-2-header.png';
+import ad3 from '../assets/ads/ad-3-header.png';
 
-export const Header = ({children}) => {
+const adsHeader = [
+  { id: 1, img: ad1 },
+  { id: 2, img: ad2 },
+  { id: 3, img: ad3 },
+];
+
+export const Header = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const changeImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % adsHeader.length);
+  };
+
+  useEffect(() => {
+    const id = setInterval(changeImage, 3000); 
+    setIntervalId(id);
+
+    return () => {
+      clearInterval(id); 
+    };
+  }, []); 
+
+  const currentAd = adsHeader[currentIndex];
+
   return (
     <>
-    <div className='Ad-header h-80 bg-UT-orange'>
-    <img src={adHeader} alt="Logo" className="w-full h-full object-contain" />
-    </div>
-    <div className='Header bg-UT-orange h-32'>
-      <ul className='list-organizer flex justify-center space-x-20
-       items-center '>
-        <li>Inicio</li>
-        <li>Productos</li>
-        <li>Categorias</li>
-      </ul>
-    </div>
+      <div className='Ad-header h-80 bg-UT-orange'>
+        <img
+          src={currentAd.img}
+          alt={`Ad ${currentAd.id}`}
+          className='w-full h-full object-contain'
+        />
+      </div>
+      <div className='Header bg-UT-orange h-10'>
+        <ul className='list-organizer flex justify-center space-x-40 items-center'>
+          <li>Inicio</li>
+          <li>Productos</li>
+          <li>Categorías</li>
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
+
+
