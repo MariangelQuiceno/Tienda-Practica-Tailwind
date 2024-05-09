4214aebdd5ebcaeddd7c1cf85ad02834bf5b7f15
Mariángel Quiceno Largo <126704306+MariangelQuiceno@users.noreply.github.com>
2024-05-09 17:27:54 -0500

    Configurado: Card

--- src/Elements/Cards.jsx
+++ src/Elements/Cards.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import imgTest from '../assets/imgForTest/test-1.png'
+import { BsFillStarFill } from "react-icons/bs";
 
 export const Cards = () => {
   return (
-    <div className='card-creator bg-white max-h-fit p-5 flex flex-row' style={{ width: '40rem' }}>
+    <div className='card-creator bg-white max-h-fit p-5 flex flex-row' style={{ width: '35rem' }}>
         <div className="img-card">
             <img
           src={imgTest}
@@ -18,14 +19,18 @@ export const Cards = () => {
             <div className="price">
               ${109.90}
             </div>
-            <div className="description w-60 h-40 text-balance">
+            <div className="description w-60 h-40">
+                <p className='desc-paragraph line-clamp-6 text-sm'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed enim in urna posuere malesuada. Nam mi sapien, posuere at laoreet tristique, ullamcorper ac justo. Etiam quis ex at erat fringilla mollis. Nulla fringilla lacus at maximus aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam vel metus eget diam porta ornare. Etiam vehicula elit at erat mattis, eleifend dignissim erat facilisis. Nullam eu ipsum arcu.
                 Curabitur turpis erat, volutpat eget ultricies sed, accumsan non neque. Sed cursus vel elit ut consectetur. Quisque ut nulla euismod, placerat libero non, ultricies arcu. Nulla ornare scelerisque enim quis volutpat. Nam non lacus non ligula rutrum rutrum. Suspendisse quam lorem, tincidunt sed suscipit id, lobortis eget dui. Phasellus feugiat blandit odio et aliquet. 
+                </p>
             </div>
+            <div className="rating flex mt-2">
+              <div className="rate font-bold text-lg">3.9 </div> <BsFillStarFill className='Star text-yellow-400 flex place-self-center ml-1' />
+              <div className="count ml-2 text-gray-500 text-sm">120 votos</div>
+            </div>
+            <button className='btn-add-to-car bg-UT-orange  hover:bg-Selective-Yellow mt-2 ml-2 rounded text-white h-7  '>Agregar al Carro</button>
         </div>
-        
-        
-        
     </div>
   )
 }
